f9e036b60d55b48e69f21492c880820344d19678
Stan-i-Slavishe <stassilin1984@gmail.com>
2025-09-21 04:59:33 +0500

    Добавлена настройка STATICFILES_DIRS в production настройки

--- config/settings_production.py
+++ config/settings_production.py
@@ -46,6 +46,7 @@ SERVER_EMAIL = DEFAULT_FROM_EMAIL
 # 📁 Static & Media Files
 STATIC_URL = '/static/'
 STATIC_ROOT = '/app/staticfiles/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
